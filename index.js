79b0d0a21cfc2794bdecfa515ba9431d35865f33
Rahul Kumar <rahulpro886@gmail.com>
2025-10-13 07:59:51 +0530

    try to correct

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ import Notes from './models/Notes.js'
 
 dotenv.config()
 const app = express()
-app.use(express.json())
 app.use(cors({
     origin: ["http://localhost:3000", "https://inotebook-client-pi.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
+app.use(express.json())
 app.use(express.static('public'))
 app.use('/api/auth', userRouter)
 app.use('/api/notes', noteRouter)
